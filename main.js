ed541a4db20d932813f2e150d8cf6c19e9e70bc9
luis rodriguez <luiseliasrodrigueztorres@gmail.com>
2024-12-03 08:21:01 -0500

    agregando funciones de js

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 console.log("Estoy en js")
+let $resultado = document.getElementById("resul")
+
+function fsumar(){
+  let num1 = parseInt(n1.value)
+  let num2 = parseInt(n2.value)
+  //console.log(typeof(num1))
+  $resultado.innerHTML = "la suma es:"+ (num1+num2)
+
+}
+function frestar(){
+    let num1 = parseInt(n1.value)
+    let num2 = parseInt(n2.value)
+    $resultado.innerHTML = "la resta es:"+(num1-num2)
+  }
+  function fmultiplicar(){
+    let num1 = parseInt(n1.value)
+    let num2 = parseInt(n2.value)
+    $resultado.innerHTML ="la multiplicacion es:"+(num1*num2)
+  } 
+  function fdividir(){
+    let num1 = parseInt(n1.value)
+    let num2 = parseInt(n2.value)
+    if(num2===0)
+    {console.log ("la divicion entre coro (0) no es posible")}
+    else{$resultado.innerHTML ="la divicion es:"+(num1/num2)}
+  }
